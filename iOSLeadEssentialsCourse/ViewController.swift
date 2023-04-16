b9973740d41c353b56724c2efda0638be2fff201
Balázs Holló <holbalzs@gmail.com>
2023-04-16 12:58:27 +0200

    init

---
+++ iOSLeadEssentialsCourse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSLeadEssentialsCourse
+//
+//  Created by Holló Balázs on 2023. 04. 16..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
