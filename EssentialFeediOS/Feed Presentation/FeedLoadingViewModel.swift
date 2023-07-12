f42e8357972b11bc9eabe8e580ca14d0976fdb51
Balázs Holló <holbalzs@gmail.com>
2023-07-12 20:20:04 +0200

    MVP changes. Remove view model, add presentation group. Feedimagepresenter with logic

---
+++ EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Holló Balázs on 2023. 07. 12..
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Holló Balázs on 2023. 07. 12..
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
