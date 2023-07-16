48dca4daf2732f24306adacdc74ce4a092dbd18a
Balázs Holló <holbalzs@gmail.com>
2023-07-16 14:09:29 +0200

    empty essential app

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Holló Balázs on 2023. 07. 16..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
